b3edb1789e0f756e97371df802101748f59ed344
Asma RACHIDI <rachidi.asm@gmail.com>
2026-09-06 14:06:45 +0200

    feat(api): ajoute les endpoints GET taches et taches par id

--- 00FrontFixe/backend/main.py
+++ 00FrontFixe/backend/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def tache_racine():
-    return {"message": "Bienvenue sur l'API FastAPI!"}
+@app.get("/taches/")
+def get_all_taches():
+    return {"message": "Fonction qui va récupérer toutes les tâches"}
+
+@app.get("/taches/{id}")
+def get_tache_by_id(id: int):
+    return {"message": f"Fonction qui va récupérer la tâche avec l'ID {id}"}
